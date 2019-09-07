106ab3de3dd53c36f35f0ee418d895eb95da3362
Simon Zhang <muyezhang@gmail.com>
2019-09-07 23:28:11 +0800

    commit

---
+++ public_prefix.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+__author__ = 'Simon Zhang'
+__date__ = '2019/9/7 23:27'
+
+#
+# 编写一个函数来查找字符串数组中的最长公共前缀。
+#
+# 如果不存在公共前缀，返回空字符串 ""。
+#
+# 示例 1:
+#
+# 输入: ["flower","flow","flight"]
+# 输出: "fl"
+# 示例 2:
+#
+# 输入: ["dog","racecar","car"]
+# 输出: ""
+# 解释: 输入不存在公共前缀。
+# 说明:
+#
+# 所有输入只包含小写字母 a-z 。
+
+def longestCommonPrefix(self, strs):
+    if not strs: return ""
+    s1 = min(strs)
+    s2 = max(strs)
+    for i, x in enumerate(s1):
+        if x != s2[i]:
+            return s2[:i]
+    return s1
