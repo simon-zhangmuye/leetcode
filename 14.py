b8ec989fe4856599774f3c9c6e203660b08fab89
Simon Zhang <muyezhang@gmail.com>
2019-09-12 01:02:19 +0800

    commit

---
+++ 14.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+__author__ = 'Simon Zhang'
+__date__ = '2019/9/12 1:01'
+
+
+# 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+#
+# 示例:
+#
+# 输入: [-2,1,-3,4,-1,2,1,-5,4],
+# 输出: 6
+# 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+# 进阶:
+#
+# 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        tmp = nums[0]
+        max_ = tmp
+        n = len(nums)
+        for i in range(1,n):
+            # 当当前序列加上此时的元素的值大于tmp的值，说明最大序列和可能出现在后续序列中，记录此时的最大值
+            if tmp + nums[i]>nums[i]:
+                max_ = max(max_, tmp+nums[i])
+                tmp = tmp + nums[i]
+            else:
+            #当tmp(当前和)小于下一个元素时，当前最长序列到此为止。以该元素为起点继续找最大子序列,
+            # 并记录此时的最大值
+                max_ = max(max_, tmp, tmp+nums[i], nums[i])
+                tmp = nums[i]
+        return max_
