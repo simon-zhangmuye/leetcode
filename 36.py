e8cb29448841c1ccfde60621d01ac87830dead1d
Simon Zhang <muyezhang@gmail.com>
2019-09-20 03:49:56 +0800

    commit

---
+++ 36.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+__author__ = 'Simon Zhang'
+__date__ = '2019/9/20 3:49'
+
+
+# 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+#
+# push(x) -- 将元素 x 推入栈中。
+# pop() -- 删除栈顶的元素。
+# top() -- 获取栈顶元素。
+# getMin() -- 检索栈中的最小元素。
+# 示例:
+#
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin();   --> 返回 -3.
+# minStack.pop();
+# minStack.top();      --> 返回 0.
+# minStack.getMin();   --> 返回 -2.
+
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+    def pop(self) -> None:
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+    def top(self) -> int:
+        return self.stack[-1]
+    def getMin(self) -> int:
+        return self.min_stack[-1]
