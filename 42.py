0caa3d8bff35594b7b288ccc4b4300320233bcaa
Simon Zhang <muyezhang@gmail.com>
2019-09-24 03:17:21 +0800

    commit

---
+++ 42.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+__author__ = 'Simon Zhang'
+__date__ = '2019/9/24 3:16'
+
+
+# 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+#
+# 说明：
+#
+# 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+#
+# 示例 1:
+#
+# 输入: [2,2,1]
+# 输出: 1
+# 示例 2:
+#
+# 输入: [4,1,2,1,2]
+# 输出: 4
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        no_duplicate_list = []
+        for i in nums:
+            if i not in no_duplicate_list:
+                no_duplicate_list.append(i)
+            else:
+                no_duplicate_list.remove(i)
+        return no_duplicate_list.pop()
