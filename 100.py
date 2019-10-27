4585a473d9d5b7e19e4d8601725b7510649ca422
Simon Zhang <muyezhang@gmail.com>
2019-10-27 13:25:16 +0800

    commit

---
+++ 100.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+__author__ = 'Simon Zhang'
+__date__ = '2019/10/27 13:24'
+
+
+# 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+#
+# 你的算法时间复杂度必须是 O(log n) 级别。
+#
+# 如果数组中不存在目标值，返回 [-1, -1]。
+#
+# 示例 1:
+#
+# 输入: nums = [5,7,7,8,8,10], target = 8
+# 输出: [3,4]
+# 示例 2:
+#
+# 输入: nums = [5,7,7,8,8,10], target = 6
+# 输出: [-1,-1]
+
+class Solution:
+    def searchRange(self, nums, target):
+        # find the index of the leftmost appearance of `target`. if it does not
+        # appear, return [-1, -1] early.
+        for i in range(len(nums)):
+            if nums[i] == target:
+                left_idx = i
+                break
+        else:
+            return [-1, -1]
+
+        # find the index of the rightmost appearance of `target` (by reverse
+        # iteration). it is guaranteed to appear.
+        for j in range(len(nums)-1, -1, -1):
+            if nums[j] == target:
+                right_idx = j
+                break
+
+        return [left_idx, right_idx]
