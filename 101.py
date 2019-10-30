e6a8d4c3856d380fe764473baece962b521459ee
Simon Zhang <muyezhang@gmail.com>
2019-10-30 11:26:24 +0800

    simon

---
+++ 101.py
@@ -0,0 +1,72 @@
+'''
+-*- coding: utf-8 -*-
+@Author  : Simon Zhang
+@Time    : 2019/10/30 11:24
+@Software: PyCharm
+@File    : 101.py
+'''
+
+# 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+#
+# candidates 中的数字可以无限制重复被选取。
+#
+# 说明：
+#
+# 所有数字（包括 target）都是正整数。
+# 解集不能包含重复的组合。 
+# 示例 1:
+#
+# 输入: candidates = [2,3,6,7], target = 7,
+# 所求解集为:
+# [
+#   [7],
+#   [2,2,3]
+# ]
+# 示例 2:
+#
+# 输入: candidates = [2,3,5], target = 8,
+# 所求解集为:
+# [
+#   [2,2,2,2],
+#   [2,3,3],
+#   [3,5]
+# ]
+
+
+
+from typing import List
+
+
+# 不排序的写法，只有遇到减到负数的时候剪枝
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        size = len(candidates)
+        if size == 0:
+            return []
+
+        res = []
+        self.__dfs(candidates, size, 0, [], target, res)
+        return res
+
+    def __dfs(self, candidates, size, start, path, residue, res):
+        if residue < 0:
+            return
+        if residue == 0:
+            res.append(path[:])
+            return
+
+        for index in range(start, size):
+            path.append(candidates[index])
+            # 注意：因为数字可以无限制重复被选取，因此起始位置还是自己
+            self.__dfs(candidates, size, index, path, residue - candidates[index], res)
+            path.pop()
+
+
+if __name__ == '__main__':
+    candidates = [2, 3, 6, 7]
+    target = 7
+    solution = Solution()
+    result = solution.combinationSum(candidates, target)
+    print(result)
+
