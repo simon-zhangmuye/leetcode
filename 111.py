66d937f8328e9e75abc39c23bc8e61de34a433f7
simonzhang <muyezhang@gmail.com>
2019-11-07 14:39:44 +0800

    simon

---
+++ 111.py
@@ -0,0 +1,32 @@
+'''
+-*- coding: utf-8 -*-
+@Author  : Simon Zhang
+@Time    : 2019/11/7 14:38
+@Software: PyCharm
+@File    : 111.py
+'''
+
+
+# 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+#
+# 示例:
+#
+# 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+# 输出:
+# [
+#   ["ate","eat","tea"],
+#   ["nat","tan"],
+#   ["bat"]
+# ]
+# 说明：
+#
+# 所有输入均为小写字母。
+# 不考虑答案输出的顺序。
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
